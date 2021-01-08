53d3ec4206e08817223657fd4063d3e483bbe9df
Jet Chien <67193495+jet-chien@users.noreply.github.com>
2021-01-08 15:51:56 +0800

    fix rotator's import

--- Rotate/rotator.py
+++ Rotate/rotator.py
@@ -8,8 +8,8 @@ from typing import Union
 
 import numpy as np
 
-from Facer.Rotate.face_rotator import FaceRotator
-from Facer.ult.read_img import get_img_arr
+from .face_rotator import FaceRotator
+from ..ult.read_img import get_img_arr
 
 
 def get_rotated_face(img: Union[str, np.ndarray]) -> Union[np.ndarray, None]:
