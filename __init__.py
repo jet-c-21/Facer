f60e62c9cb84ca29add9bf357dc108ac4c6d04af
Jet Chien <67193495+jet-chien@users.noreply.github.com>
2021-01-11 00:30:04 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,3 +4,6 @@ GitHub: https://github.com/jet-chien
 Create Date: 2021/1/8
 """
 # coding: utf-8
+from .Detect.face_capturer import FaceCapturer
+from .Detect.lmk_scanner import LMKScanner
+from .Recognize.adam_geitgey import AGFaceRecog
