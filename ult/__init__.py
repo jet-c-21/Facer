fe05012835ef68aa6ea9c8567a812d5ac1ed970f
Jet Chien <67193495+jet-chien@users.noreply.github.com>
2021-01-13 17:26:53 +0800

    Update __init__.py

--- ult/__init__.py
+++ ult/__init__.py
@@ -5,6 +5,6 @@ Create Date: 2021/1/8
 """
 # coding: utf-8
 from .data_store import save_as_pkl, load_pkl, save_as_json, load_json
-from .read_img import get_img_arr
+from .read_img import get_img_ls, get_img_arr
 from .view import show
 from .model_api import ModelAPI
